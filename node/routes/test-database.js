4670c8e4c9269faa5ce5a00b29df69d53f03c1f5
s1260150 <s1260150@u-aizu.ac.jp>
2020-12-11 17:12:13 +0900

    map test

--- node/routes/test-database.js
+++ node/routes/test-database.js
@@ -1,26 +1,9 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host: '172.27.0.2',
-    user: 'user',
-    password: 'user',
-    port: 3306,
-    database: 'test_database'
-    });
-
-connection.connect((err) => {
-    if (err) {
-        console.log('error connecting: ' + err.stack);
-        throw err;
-    }
-    console.log('success');
-});
-
-
+const db = require('../my_modules/mydb');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    connection.query('SELECT * FROM test_table', (error, results) => 
+    db.query('SELECT * FROM test_table', (error, results) => 
     {
         let user;
         console.log(results);
@@ -28,8 +11,6 @@ router.get('/', function(req, res, next) {
         console.log(user);
         res.render('test-database', { title: user });
     });
-
-
 });
 
 
