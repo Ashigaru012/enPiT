04a19c1d9549d92d6b831e41a8d719caaad70dd6
s1260150 <s1260150@u-aizu.ac.jp>
2020-12-13 22:06:54 +0900

    modify test-database

--- node/routes/test-database.js
+++ node/routes/test-database.js
@@ -1,25 +1,5 @@
-<<<<<<< HEAD
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host: '172.27.0.2',
-    user: 'user',
-    password: 'user',
-    port: 3306,
-    database: 'test_database'
-    });
 
-connection.connect((err) => {
-    if (err) {
-        console.log('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('success');
-});
-
-
-=======
 const db = require('../my_modules/mydb');
->>>>>>> 6f1d76beb10412d76af52f6243fefdc1ff63a38a
 const express = require('express');
 const router = express.Router();
 
