24d1bb41293f464181b50a172cb792f225de9a98
Miyaoke <48222230+Miyaoke@users.noreply.github.com>
2021-01-12 00:15:08 +0900

    Made login function
    Checks if phone number and code exists in firebase

---
+++ node/html/miyaoke/fbClient.js
@@ -0,0 +1,13 @@
+if (!firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey: "AIzaSyCMLrGQY7Bf1V7RqkMD_gl7RAg8ohAr5Do",
+        authDomain: "taskul.firebaseapp.com",
+        projectId: "taskul",
+        storageBucket: "taskul.appspot.com",
+        messagingSenderId: "968931072741",
+        appId: "1:968931072741:web:b88bb8ad10ca17bc9fd20c",
+        measurementId: "G-KREZKMN9JY"
+    })
+}
+
+export default firebase
