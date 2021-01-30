d6260ecc82a3217d0b5c008c4154587fd88dd37a
s1260150 <s1260150@u-aizu.ac.jp>
2021-01-30 10:15:49 +0900

    modify chat functionality

--- node/routes/rooms.js
+++ node/routes/rooms.js
@@ -45,7 +45,7 @@ router.get('/:room_id', async function(req, res, next)
         
         const room = (await tr.query('select * from chat_room where id=?', [room_id]))[0];
 
-        const user = (await tr.query('select * from users where id=?', [user_id]))[0];
+        const user = (await tr.query('select * from user where id=?', [user_id]))[0];
 
         res.render('rooms/chat', {common: common, room_title: room.title, room_id: room.id, user_id: user.id, host_id: room.host_id, user: user});
     }
@@ -56,107 +56,102 @@ router.get('/:room_id', async function(req, res, next)
 });
 
 
-/*
-router.post('/good', function(req, res, next) 
+router.post('/chat/good', async function(req, res, next) 
 {
-    const good = req.body;
-
-    console.log(good);
-    
-    if(good.host_id == good.user_id)
+    try
     {
-        db.query('select * from chat_members where room_id=? and user_id!=?', [good.room_id, good.host_id], (error, results) => 
-        {
-            if(error) throw error;
+        const good = req.body;
 
-            const chat_member_ids = results.map(v => v.user_id);
-            if(chat_member_ids.length)
+        logger.info("good - ", good);
+        
+        if(good.host_id == good.user_id)
+        {
+            const results = await tr.query('select * from chat_member where room_id=? and user_id!=?', [good.room_id, good.host_id]);
+
+            const member_ids = results.map(v => v.user_id);
+
+            if(member_ids.length)
             {
-                db.query('update users set good=good+1 where id in(?)', [chat_member_ids], (error, results) => 
-                {
-                    if(error) throw error;
-
-                    res.json(good);
-                });
+                await tr.query('update user set good=good+1 where id in(?)', [member_ids]);
+                
+                res.json(good);
             }
-        });
-    }
-    else
-    {
-        db.query('update users set good=good+1 where id=?', [good.host_id], (error, results) => 
+        }
+        else
         {
-            if(error) throw error;
-
+            await tr.query('update user set good=good+1 where id=?', [good.host_id]);
+            
             res.json(good);
-        });
+        }
+    }
+    catch(err)
+    {
+        logger.error(err);
     }
 });
 
-
 const connects = new Set();
-router.ws('/', (ws, req) => {
+router.ws('/chat-web-sock', (ws, req) => {
     connects.add(ws);
-    console.log("new connection");
-    
-    ws.on('message', json => 
-    {
-        console.log('Received -', json);
 
-        const msg = JSON.parse(json);
-        if(msg.key == "on_connected")
+    logger.info("new connection - chat");
+    
+    ws.on('message', async json => 
+    {
+        try
         {
-            db.query('select * from chat_messages where room_id=?', [msg.room_id], (error, results) => 
+            logger.info('Received - chat: ', json);
+
+            const msg = JSON.parse(json);
+            if(msg.key == "on_connected")
             {
-                if(error) throw error;
-        
-                for(message of results)
+                const results = await tr.query('select * from chat_message where room_id=?', [msg.room_id]);
+
+                for(const message of results)
                 {
                     ws.send(JSON.stringify(message));
                 }
-            });
-        }
-        else if(msg.key == "submit")
-        {
-            db.query('insert into chat_messages set ?', [msg], (error, results) => 
+            }
+            else if(msg.key == "submit")
             {
-                if(error) throw error;
+                await tr.query('insert into chat_message set ?', [msg]);
+                
+                for(const socket of connects)
+                {
+                    socket.send(JSON.stringify(msg));
+                };
+            }
+            else if(msg.key == "request_location")
+            {
+                const user = (await tr.query('select * from user where id=?', [msg.user_id]))[0];
+                
+                msg.message = `https://www.google.com/maps?q=${user.lat},${user.lng}`;
+                
+                await tr.query('insert into chat_message set ?', [msg]);
 
                 for(const socket of connects)
                 {
                     socket.send(JSON.stringify(msg));
                 };
-            });
+            }
         }
-        else if(msg.key == "request_location")
+        catch(err)
         {
-            db.query('select * from users where id=?', [msg.user_id], (error, results) => 
-            {
-                if(error) throw error;
-
-                const user = results[0];
-
-                msg.message = `https://www.google.com/maps?q=${user.lat},${user.lng}`;
-                
-                db.query('insert into chat_messages set ?', [msg], (error, results) => 
-                {
-                    if(error) throw error;
-
-                    for(const socket of connects)
-                    {
-                        socket.send(JSON.stringify(msg));
-                    };
-                });
-                
-            });
+            logger.error(err);
         }
     });
   
     ws.on('close', () => 
     {
-        connects.delete(ws);
+        try
+        {
+            connects.delete(ws);
+        }
+        catch(err)
+        {
+            logger.error(err);
+        }
     });
 });
-
-*/
 
 module.exports = router;
